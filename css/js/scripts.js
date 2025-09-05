e924ad5983ae79ab4ca0df667601e976376f3064
gunduboyinapawansai-stack <gunduboyinapawansai@gmail.com>
2025-09-05 14:49:55 +0530

    first commit

---
+++ css/js/scripts.js
@@ -0,0 +1,9 @@
+// Simple JavaScript example
+
+// When the page loads, show a welcome message in the console
+console.log("Welcome to My Website!");
+
+// Example: toggle dark mode
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
